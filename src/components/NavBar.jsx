72890c9be6e6dde156a493fc67f79156546bd055
chamiru.uuu <chamiru.codes@gmail.com>
2025-03-18 17:27:52 +0530

    null commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
@@ -21,7 +21,7 @@ const NavBar = () => {
 
     return (
         <nav className={`${styles.navbar} ${isCollapsed ? styles.collapsed : ''}`} id="navbar">
-            <div className={`${styles.logo} ${isCollapsed ? styles.hidden : ''}`} id="logo">chamirurf</div>
+            <div className={`${styles.logo} ${isCollapsed ? styles.hidden : ''}`} id="logo"><img src="src/assets/logo.svg" /></div>
             <ul className={styles.navLinks} id="nav-links">
                 <li><a href="#">HOME</a></li>
                 <li><a href="#">ABOUT</a></li>
@@ -29,8 +29,8 @@ const NavBar = () => {
                 <li><a href="#">CONTACT</a></li>
             </ul>
             <div className={`${styles.iconButtons} ${isCollapsed ? styles.hidden : ''}`} id="icon-buttons">
-                <button className={styles.iconBtn} id="theme-toggle">🌓</button>
-                <button className={styles.iconBtn} id="language-toggle">🌐</button>
+                <button className={styles.iconBtn} id="theme-toggle"><img src="src/assets/light_mode.svg" /></button>
+                <button className={styles.iconBtn} id="language-toggle"><img src="src/assets/language_us.svg" /></button>
             </div>
         </nav>
     );
